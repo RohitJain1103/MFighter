79cfb5b503037289dc7f26f75f40a797a305bf5c
suresh-gandhi <sureshgandhi200@gmail.com>
2018-05-31 22:21:12 +0530

    Done with creating and joining the server

---
+++ Assets/Scripts/PhotonButtonsBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotonButtonsBehaviour : MonoBehaviour {
+
+    [SerializeField]
+    private InputField createServerInputField, joinServerInputField;
+
+    public void OnClick_CreateServer(){
+        // grab the text from the createServerInputField(check for blank text etc etc)
+        if (createServerInputField.text.Length > 0) {
+            PhotonNetwork.CreateRoom(createServerInputField.text, new RoomOptions() { MaxPlayers = 4 }, null);
+        }
+    }
+
+    public void OnClick_JoinServer() {
+        PhotonNetwork.JoinRoom(joinServerInputField.text);
+    }
+
+    private void OnJoinedRoom(){
+        Debug.Log("We are connected to the room");
+        PhotonNetwork.LoadLevel("MainGame");
+    }
+
+}
+
+
+/*
+ *      TODO:
+ *      1. Check for the server name length. The user may do weird stuff sometimes. 
+ *      2. For LoadLevel think of making a separate script. He did it!
+ * 
+ *      DOUBT:
+ *      1. Check if the same named server is already present. If it is prompt the user to join the server. Why didn't
+ *      the guy incorporate this.
+ *      
+ *      2. Research more about the lobby names and the default lobby thing!
+ * 
+ */
---
+++ Assets/Scripts/PhotonConnect.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonConnect : MonoBehaviour {
+
+    string versionName = "0.1";
+
+    [SerializeField]
+    private GameObject section1, section2, section3;
+
+    public void Awake(){
+        Debug.Log("Sending the request to the photon server");   // Debug sending request
+        PhotonNetwork.ConnectUsingSettings(versionName);         // Send the request to photon server with the versionName variable
+    }
+
+    // PHOTON CALLBACK 
+    private void OnConnectedToMaster() {
+        Debug.Log("Connected to the master");                    // Debug connected to the master
+        PhotonNetwork.JoinLobby(TypedLobby.Default);             // join the lobby
+    }
+
+    // PHOTON CALLBACK
+    private void OnJoinedLobby() {
+        Debug.Log("On Joined the lobby");                        // Debug the lobby is joined
+        section1.SetActive(false);
+        section3.SetActive(false);
+        section2.SetActive(true);
+    }
+
+    // PHOTON CALLBACK
+    private void OnDisconnectedFromPhoton() {
+        Debug.Log("Disconnected from the photon services.");    // Debug disconnected from the photon services check your internet connection perhaps
+        section1.SetActive(false);
+        section2.SetActive(false);
+        section3.SetActive(true);
+    }
+
+}
+
+
+
+/*
+ * TODO:
+ * 1. Handle the logic when the connection gets re-established(Also keep care of saving the game state as well)
+ * 2. Do something during the period when the client is about to lose the connection
+ * 3. Similarly do something during the period when we are just about to connect
+ */
+  
+
+    /* DOUBT:
+     * 1. Does the photon callback gets called everywhere possible(searches everywhere) or on the gameobject which calls it?
+     * 
+     */ 
